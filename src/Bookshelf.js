915bc11044d97eafc7a16f5b4dc909c360a38aa8
Henrique Silva <hhsilva@thoughtworks.com>
2018-08-03 18:26:36 -0300

    feat: Create Book component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Book from './Book'
 import PropTypes from 'prop-types'
 
 class Bookshelf extends Component {
@@ -6,35 +7,36 @@ class Bookshelf extends Component {
     books: PropTypes.array.isRequired
   }
 
+  getBooksByBookshelf = (books, shelf) => {
+    return books.filter((book) => book.shelf === shelf)
+  }
+
   render () {
     const { books } = this.props
+
+    let bookshelves = [
+      { title: 'Currently Reading', books: this.getBooksByBookshelf(books, 'currentlyReading') },
+      { title: 'Want to Read', books: this.getBooksByBookshelf(books, 'wantToRead') },
+      { title: 'Read', books: this.getBooksByBookshelf(books, 'read') }
+    ]
 
     return (
       <div>
         <div className="bookshelf">
-          <h2 className="bookshelf-title">Currently Reading</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              <li>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">To Kill a Mockingbird</div>
-                  <div className="book-authors">Harper Lee</div>
-                </div>
-              </li>
-            </ol>
-          </div>
+          {bookshelves.map((bookshelf, index) => (
+            <div key={index}>
+              <h2 className="bookshelf-title">{bookshelf.title}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {bookshelf.books.map((book) => (
+                    <li key={book.id}>
+                      <Book book={book}/>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     )
